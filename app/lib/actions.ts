e3fac3d7f7a5a6584fddd338e9aaf026863d97d0
truongtuankiet1 <lop5adoican@gmail.com>
2026-02-23 15:03:50 +0700

    fix: update db connection for Vercel

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,7 +8,10 @@ export async function authenticate(
     formData: FormData,
 ) {
     try {
-        await signIn('credentials', formData);
+        await signIn('credentials', {
+            ...Object.fromEntries(formData),
+            redirectTo: '/dashboard',
+        });
     } catch (error) {
         if (error instanceof AuthError) {
             const cause = error.cause as any;
